78246a9e144281b89c1ca457b0d220cd4f6b1dc3
Payton Clark <payton182@gmail.com>
2018-12-29 12:31:58 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+//load Handlebars and set static directory
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+//page logger middleware
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = (`${now}: ${req.method} ${req.url}`);
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  });
+  next();
+});
+
+//Override render for maintenence
+app.use((req, res, next) => {
+  res.render('maintenence.hbs');
+});
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+app.get('/home', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about-page.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Unable to handle request'
+  });
+});
+
+
+app.listen(3000);
